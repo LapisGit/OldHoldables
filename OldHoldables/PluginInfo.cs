012e4323aec16a8b95ed3359acb655717af24235
Lapis <lapiscodes@gmail.com>
2026-05-18 08:24:52 -0400

    bump ver

--- OldHoldables/PluginInfo.cs
+++ OldHoldables/PluginInfo.cs
@@ -4,7 +4,7 @@
     {
         public const string GUID = "oldholdables";
         public const string Name = "OldHoldables";
-        public const string Version = "2.4.3";
+        public const string Version = "1.0.3";
         public const string Author = "gizmogoat/dev9998/lapis";
     }
 }
